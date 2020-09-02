72fdd465de474b99728674922efb0f2629427255
Andrejs Krilovs <andrejs.krilovs.0507@gmail.com>
2020-09-02 22:47:18 +0300

    Remove illegal symbol :)

--- cinema-api/src/main/java/lv/sda/cinemaapi/controller/SessionController.java
+++ cinema-api/src/main/java/lv/sda/cinemaapi/controller/SessionController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
-p@RestController
+@RestController
 @RequestMapping("/api/session.svc")
 @CrossOrigin(origins="http://localhost:4200")
 public class SessionController {
