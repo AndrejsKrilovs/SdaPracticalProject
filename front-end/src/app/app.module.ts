3a44a76826b2946e16c65b18fe7d3f501069b870
Andrejs Krilovs <andrejs.krilovs.0507@gmail.com>
2020-09-02 16:26:11 +0300

    Frontend changes for order conformation

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -9,6 +9,7 @@ import {FilmSessionComponent} from './film-session/film-session.component';
 import {RouterModule} from '@angular/router';
 import {FilmListComponent} from './film-list/film-list.component';
 import {HttpClientModule} from '@angular/common/http';
+import {DatePipe} from "@angular/common";
 
 @NgModule({
   declarations: [
@@ -22,7 +23,7 @@ import {HttpClientModule} from '@angular/common/http';
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- front-end/src/app/film-session/film-session.component.ts
+++ front-end/src/app/film-session/film-session.component.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {IFilm, ISeat, ISession} from '../app.component';
 import {ApiService} from '../api.service';
+import {DatePipe} from "@angular/common";
 
 @Component({
   selector: 'app-film-session',
@@ -12,8 +13,8 @@ export class FilmSessionComponent {
   sessions: Array<ISession> = [];
   seats: Array<ISeat> = [];
 
-  currentPage = 0;
-  selectedSeats = 0;
+  selectedSeats: number = 0;
+  currentPage: number = 0;
 
   selectedSession: ISession = {
     id: 0,
@@ -30,7 +31,15 @@ export class FilmSessionComponent {
     title: ''
   };
 
-  constructor(private route: ActivatedRoute, private apiService: ApiService) {
+  order: any = {
+    generationTime: '',
+    film: null,
+    session: 0,
+    places: [],
+    totalPrice: 0
+  };
+
+  constructor(private route: ActivatedRoute, private apiService: ApiService, public datePipe: DatePipe) {
     this.selectedFilm.id = Number.parseInt(this.route.snapshot.paramMap.get('id'), 0);
     apiService.oneFilm(this.selectedFilm.id)
       .subscribe(result => this.selectedFilm = result);
@@ -42,40 +51,60 @@ export class FilmSessionComponent {
   private init(): void {
       this.sessions.splice(0);
       this.selectedSession.id = 0;
-      this.selectedSeats = 0;
   }
 
   onSessionSelect(session: ISession): void {
-    this.selectedSeats = 0;
     this.seats.splice(0);
     this.seats.forEach(s => s.available = false);
     this.selectedSession = session;
 
     this.apiService.showSeats(this.selectedSession.room)
-      .subscribe(result => result.forEach(data => this.seats.push(data)));
+      .subscribe(result =>
+        result.forEach(data => this.seats.push(data))
+      );
   }
 
   onSeatSelect(event: any, seat: ISeat): void {
     const selectedNumber: number = Number.parseInt(event.target.text, 0);
     this.seats.forEach(_seat => {
-      if(_seat == seat) {
-        _seat.available = !_seat.available;
-        _seat.available ? this.selectedSeats++ : this.selectedSeats--;
-      }
-    });
+        if(_seat == seat) {
+          _seat.available = !_seat.available;
+          seat = _seat;
+
+          if(seat.available) {
+            ++ this.selectedSeats;
+            this.order.places.push(seat);
+          } else if(!seat.available) {
+            const index = this.order.places.indexOf(seat);
+            this.order.places.splice(index, 1);
+            -- this.selectedSeats;
+          }
+        }
+      });
   }
 
-  pageUp() {
+  pageUp(): void {
     this.init();
     this.currentPage > 0 ? this.currentPage = 1 : this.currentPage ++ ;
     this.apiService.sessionCollection(this.selectedFilm.id, this.currentPage)
       .subscribe(result => result.forEach(data => this.sessions.push(data)));
   }
 
-  pageDown() {
+  pageDown(): void {
     this.init();
     this.currentPage <= 0 ? this.currentPage = 0 : this.currentPage -- ;
     this.apiService.sessionCollection(this.selectedFilm.id, this.currentPage)
       .subscribe(result => result.forEach(data => this.sessions.push(data)));
+  }
+
+  makeOrder(): void {
+    this.order.generationTime = this.datePipe
+      .transform(new Date(), 'dd.MM.yyyy hh:mm:ss');
+
+    this.order.session = this.selectedSession;
+    this.order.film = this.selectedFilm;
+    this.order.totalPrice = this.selectedSeats * this.selectedSession.price;
+
+    console.log(this.order);
   }
 }
