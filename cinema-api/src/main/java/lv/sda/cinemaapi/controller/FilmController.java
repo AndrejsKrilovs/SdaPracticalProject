12622ca1ae59519a17dcbe65c0b1eaebdf660a15
akoz <kozirevs@inbox.lv>
2020-09-04 13:28:16 +0300

    Back-end changes. Add FilmService and replace all logic from FilmController to FilmService.(by Aleksandrs Kozirevs)

--- cinema-api/src/main/java/lv/sda/cinemaapi/controller/FilmController.java
+++ cinema-api/src/main/java/lv/sda/cinemaapi/controller/FilmController.java
@@ -2,39 +2,30 @@ package lv.sda.cinemaapi.controller;
 
 import lv.sda.cinemaapi.dto.FilmDTO;
 import lv.sda.cinemaapi.entity.Film;
-import lv.sda.cinemaapi.mapper.FilmMapper;
-import lv.sda.cinemaapi.repository.FilmRepository;
-import org.springframework.beans.BeanUtils;
-import org.springframework.data.domain.PageRequest;
+import lv.sda.cinemaapi.service.FilmService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/film.svc")
 @CrossOrigin(origins="http://localhost:4200")
 public class FilmController {
-    private final FilmRepository filmRepository;
-    private final FilmMapper filmMapper;
 
-    public FilmController(FilmRepository filmRepository, FilmMapper filmMapper) {
-        this.filmRepository = filmRepository;
-        this.filmMapper = filmMapper;
+    private final FilmService filmService;
+
+    public FilmController(FilmService filmService) {
+        this.filmService = filmService;
     }
 
     @GetMapping("/Films")
-    public List<FilmDTO> findAll(
-            @RequestParam(value = "offset", required = false, defaultValue = "0") Integer offset
-    ) {
-        return filmRepository.findAll(PageRequest.of(offset, 10))
-                .stream()
-                .map(filmMapper::toDTO)
-                .collect(Collectors.toList());
+    public List<FilmDTO> findAll(@RequestParam(value = "offset", required = false, defaultValue = "0")
+                                             Integer offset) {
+        return filmService.getFilms(offset);
     }
 
     @GetMapping("/Film({id})")
     public FilmDTO getOne(@PathVariable("id") Film film) {
-        return filmMapper.toDTO(film);
+        return filmService.getFilm(film);
     }
 }
---
+++ cinema-api/src/main/java/lv/sda/cinemaapi/service/FilmService.java
@@ -0,0 +1,34 @@
+package lv.sda.cinemaapi.service;
+
+import lv.sda.cinemaapi.dto.FilmDTO;
+import lv.sda.cinemaapi.entity.Film;
+import lv.sda.cinemaapi.mapper.FilmMapper;
+import lv.sda.cinemaapi.repository.FilmRepository;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class FilmService {
+
+    private final FilmRepository filmRepository;
+    private final FilmMapper filmMapper;
+
+    public FilmService(FilmRepository filmRepository, FilmMapper filmMapper) {
+        this.filmRepository = filmRepository;
+        this.filmMapper = filmMapper;
+    }
+
+    public List<FilmDTO> getFilms(Integer offset) {
+        return filmRepository.findAll(PageRequest.of(offset, 10))
+                .stream()
+                .map(filmMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+
+    public FilmDTO getFilm(Film film) {
+        return filmMapper.toDTO(film);
+    }
+}
