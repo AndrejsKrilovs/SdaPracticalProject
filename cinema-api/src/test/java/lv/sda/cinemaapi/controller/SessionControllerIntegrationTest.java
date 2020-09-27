997249f3bcdf7e0224de7e6163932f6fc67481c4
Andrejs Krilovs <andrejs.krilovs.0507@gmail.com>
2020-09-27 15:10:41 +0300

    Backend changes. Finished integration tests. Refactored code.

--- cinema-api/src/test/java/lv/sda/cinemaapi/controller/SessionControllerIntegrationTest.java
+++ cinema-api/src/test/java/lv/sda/cinemaapi/controller/SessionControllerIntegrationTest.java
@@ -10,10 +10,12 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.Objects;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SessionControllerIntegrationTest {
 
-    private final static String URL_TO_TEST = "http://localhost:%d/api/film.svc%s";
+    private final static String URL_TO_TEST = "http://localhost:%d/api/session.svc/Sessions(%d)";
     private final static Integer ITEMS_PER_PAGE = 7;
 
     @LocalServerPort
@@ -23,17 +25,18 @@ public class SessionControllerIntegrationTest {
     private TestRestTemplate restTemplate;
 
     @Test
-    public void findSessionByFilmTest() {
-        String url = String.format(URL_TO_TEST, port, "/Sessions(1)");
+    public void findSessionsByFilmTest() {
+        String url = String.format(URL_TO_TEST, port, Integer.SIZE);
         ResponseEntity<SessionDTO[]> entity = restTemplate.getForEntity(url, SessionDTO[].class);
         Assertions.assertEquals(HttpStatus.OK, entity.getStatusCode());
+        Assertions.assertEquals(ITEMS_PER_PAGE, Objects.requireNonNull(entity.getBody()).length);
     }
-//
-//    @Test
-//    public void findSessionByFilmWithIncorrectParameterTest() {
-//        String url = String.format(URL_TO_TEST, port, "/Sessions(99999)");
-//        ResponseEntity<SessionDTO[]> entity = restTemplate.getForEntity(url, SessionDTO[].class);
-//        Assertions.assertEquals(HttpStatus.OK, entity.getStatusCode());
-//        Assertions.assertArrayEquals(new SessionDTO[]{new SessionDTO()}, entity.getBody());
-//    }
+
+    @Test
+    public void findSessionByFilmWithIncorrectParameterTest() {
+        String url = String.format(URL_TO_TEST, port, Integer.MAX_VALUE);
+        ResponseEntity<SessionDTO[]> entity = restTemplate.getForEntity(url, SessionDTO[].class);
+        Assertions.assertEquals(HttpStatus.OK, entity.getStatusCode());
+        Assertions.assertEquals(0, Objects.requireNonNull(entity.getBody()).length);
+    }
 }
