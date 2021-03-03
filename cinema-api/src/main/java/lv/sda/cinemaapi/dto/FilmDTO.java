c2025ee24ca8fdb8ff2890eb24b05cd3ca778f0f
andrejs.krilovs <andrejs,krilovs@vodafone.com>
2021-03-03 22:18:43 +0200

    Backend changes: Refactored FilmDTO model

--- cinema-api/src/main/java/lv/sda/cinemaapi/dto/FilmDTO.java
+++ cinema-api/src/main/java/lv/sda/cinemaapi/dto/FilmDTO.java
@@ -2,46 +2,22 @@ package lv.sda.cinemaapi.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
 
+import java.time.LocalTime;
+
+@Data
 public class FilmDTO {
+    @JsonProperty(value = "id")
     private Long id;
+
+    @JsonProperty(value = "title")
     private String title;
 
     @JsonProperty(value = "picture")
     private String picturePath;
 
+    @JsonProperty(value = "length")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "hh:mm")
-    private String length;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getPicturePath() {
-        return picturePath;
-    }
-
-    public void setPicturePath(String picturePath) {
-        this.picturePath = picturePath;
-    }
-
-    public String getLength() {
-        return length;
-    }
-
-    public void setLength(String length) {
-        this.length = length;
-    }
+    private LocalTime length;
 }
--- cinema-api/src/main/java/lv/sda/cinemaapi/mapper/FilmMapper.java
+++ cinema-api/src/main/java/lv/sda/cinemaapi/mapper/FilmMapper.java
@@ -4,25 +4,14 @@ import lv.sda.cinemaapi.dto.FilmDTO;
 import lv.sda.cinemaapi.entity.Film;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalTime;
-
 @Component
 public class FilmMapper {
-    public Film fromDTO(FilmDTO filmDTO) {
-        Film result = new Film();
-        result.setId(filmDTO.getId());
-        result.setTitle(filmDTO.getTitle());
-        result.setPicturePath(filmDTO.getPicturePath());
-        result.setLength(LocalTime.parse(filmDTO.getLength()));
-        return result;
-    }
-
     public FilmDTO toDTO(Film film) {
         FilmDTO result  = new FilmDTO();
         result.setId(film.getId());
         result.setTitle(film.getTitle());
         result.setPicturePath(film.getPicturePath());
-        result.setLength(film.getLength().toString());
+        result.setLength(film.getLength());
         return result;
     }
 }
