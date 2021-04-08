bb1616058a2a7e9f845194907e1e02403d9fe7a9
Andrejs Krilovs <andrejs.krilovs.0507@gmail.com>
2021-04-08 22:46:32 +0300

    Backend changes: Moved logic to service layer

--- cinema-api/src/main/java/lv/sda/cinemaapi/controller/OrderController.java
+++ cinema-api/src/main/java/lv/sda/cinemaapi/controller/OrderController.java
@@ -1,38 +1,14 @@
 package lv.sda.cinemaapi.controller;
 
 import lombok.RequiredArgsConstructor;
-import lv.sda.cinemaapi.dto.OrderDTO;
-import lv.sda.cinemaapi.dto.PreOrderDTO;
-import lv.sda.cinemaapi.dto.SessionDTO;
-import lv.sda.cinemaapi.mapper.SessionMapper;
-import lv.sda.cinemaapi.service.FilmService;
-import lv.sda.cinemaapi.service.SessionService;
-import org.springframework.web.bind.annotation.*;
-
-import java.math.BigDecimal;
-import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(path = "/api/order.svc")
 @CrossOrigin(origins = "http://localhost:4200")
 public class OrderController {
-    private final FilmService filmService;
-    private final SessionService sessionService;
-    private final SessionMapper sessionMapper;
 
-    @PostMapping(path = "/Calculation")
-    public OrderDTO generateOrderData(@RequestBody PreOrderDTO preOrderDTO) {
-        SessionDTO sessionDTO = sessionMapper.generateSession(sessionService
-                .getById(preOrderDTO.getSessionNumber()));
-
-        return OrderDTO.builder()
-                .places(preOrderDTO.getPlaces().stream().map(String::valueOf).collect(Collectors.joining(",")))
-                .totalPrice(sessionDTO.getPrice().multiply(BigDecimal.valueOf(preOrderDTO.getPlaces().size())))
-                .filmName(filmService.getFilmById(preOrderDTO.getFilmNumber()).getTitle())
-                .sessionDate(sessionDTO.getDateTime())
-                .currency(sessionDTO.getCurrency())
-                .roomNumber(sessionDTO.getRoom())
-                .build();
-    }
 }
--- cinema-api/src/main/java/lv/sda/cinemaapi/controller/PlaceController.java
+++ cinema-api/src/main/java/lv/sda/cinemaapi/controller/PlaceController.java
@@ -3,8 +3,6 @@ package lv.sda.cinemaapi.controller;
 import lombok.RequiredArgsConstructor;
 import lv.sda.cinemaapi.dto.PlaceDTO;
 import lv.sda.cinemaapi.dto.ResponseDTO;
-import lv.sda.cinemaapi.entity.Place;
-import lv.sda.cinemaapi.mapper.PlaceMapper;
 import lv.sda.cinemaapi.service.PlaceService;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,17 +12,11 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "http://localhost:4200")
 public class PlaceController {
     private final PlaceService placeService;
-    private final PlaceMapper placeMapper;
 
     @GetMapping(path = "/Places")
-    public ResponseDTO<PlaceDTO> findPlacesBySession(@RequestParam(value = "session_id") Long sessionId) {
-        return placeMapper.generateResponse(placeService.findPlacesBySession(sessionId));
-    }
-
-    @PutMapping(value = "/Place")
-    public PlaceDTO updatePlace(@RequestBody PlaceDTO placeDTO) {
-        Place placeToUpdate = placeMapper.fromDTO(placeDTO);
-        Place updatedPlace = placeService.changePlaceStatus(placeToUpdate);
-        return placeMapper.generatePlace(updatedPlace);
+    public ResponseDTO<PlaceDTO> findPlacesBySession(
+            @RequestParam(value = "session_id") Long sessionId
+    ){
+        return placeService.findPlacesBySession(sessionId);
     }
 }
